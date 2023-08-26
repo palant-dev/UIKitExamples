a4cd426f0714a2158571e1a87663a4573aed4269
palant-dev <antonio.palomba2015@gmail.com>
2023-08-26 16:57:31 +0200

    feat: sets the basic property that stores the string of the image for SV app (from now on StormViewer app will be referred as SV )

--- StormViewer/StormViewer/ViewController.swift
+++ StormViewer/StormViewer/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let fm = FileManager.default
+        // Force unwrapping this is safe because apple apps has always a resources path
+        let path = Bundle.main.resourcePath!
+        // This is a force unwrapping justified because if it crash is ok because the app is broken and should be immediately stop
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
     }
 
 
